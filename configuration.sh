353dc18f0ab5fc22a9c96420c10b146801cd948f
dymax-jp <dima@dymax.com.ua>
2025-04-12 10:11:15 +0300

    Adaptation to Kubuntu Noble(not finished)

--- configuration.sh
+++ configuration.sh
@@ -1,15 +1,10 @@
 # Kodi install configuration file.
 # No trailing / in these variables.
 
-# Kodi source directory.
 KODI_SOURCE_DIR=/home/$USER/kodi/kodi-source
-
-# Kodi old stable version using Python 2.
-# KODI_SOURCE_TAG=18.9-Leia
-# KODI_BUILD_DIR=/home/kodi/kodi-build-leia
-# KODI_INSTALL_DIR=/home/kodi/kodi-bin-leia
 
 # Kodi stable version using Python 3.
 KODI_SOURCE_TAG=21.2-Omega
+
 KODI_BUILD_DIR=/home/$USER/kodi/kodi-build
 KODI_INSTALL_DIR=/home/$USER/kodi/kodi-bin
--- update-kodi-source-code.sh
+++ update-kodi-source-code.sh
@@ -7,6 +7,6 @@ cd $KODI_SOURCE_DIR
 # Restore this file. Otherwise `git checkout` will fail because local changes
 # will be overwritten error. It is safe to execute `git restore` multiple times.
 git restore cmake/addons/bootstrap/repositories/binary-addons.txt
-git checkout master
+git checkout $KODI_SOURCE_TAG
 git pull --all
 cd $current_dir
